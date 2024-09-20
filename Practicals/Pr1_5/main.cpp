9895ca982a0834285349d0870778852e3f5f0cb4
Fanuk5000 <illyamak24@gmail.com>
2024-09-20 14:52:07 +0300

    minor

--- Practicals/Pr1_5/main.cpp
+++ Practicals/Pr1_5/main.cpp
@@ -17,13 +17,25 @@ void initRandomizer()
 	// Seed the random number generator with the current time
 	srand(time(0)); // srand(time(NULL)) could also be used
 }
-
+void coutVec(vector<int> a, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << a[i] << "[" << i << "]" << " ";
+	}
+}
 int main()
 {
 	// Задано послідовність значень А[n]. Знайти мінімальне значення у послідовності.
 	{
-		initRandomizer();
 
+		int max;
+		int sveIndxMinN;
+		int sveIndxMin;
+		int sveIndxMax;
+		bool the_world = true;
+		initRandomizer();
+		bool checks = true;
 		int n;
 		cout << "Enter the length of the sequence: ";
 		cin >> n;
@@ -37,34 +49,54 @@ int main()
 		for (int i = 0; i < n; i++)
 		{
 			// arr[i] = rand();
-			int randomVar = (rand() % 100) - 50;
+			int randomVar = (rand() % 100 - 50);
 			myVector.push_back(randomVar);
 		}
-
+		int minN = 99999;
+		int min = 99999;
 		// cout the array
+		coutVec(myVector, n);
+		cout << endl;
+		// calculation of the array
 		for (int i = 0; i < n; i++)
 		{
-			cout << myVector[i] << " ";
-		}
-		cout << endl;
-
-		int min = myVector[0];
-		for (int i = 1; i < n; i++)
-		{
+			if (p == myVector[i] and the_world)
+			{
+				cout << "The index of first " << p << " is " << i << endl;
+				checks = false;
+				the_world = false;
+			}
+			if (myVector[i] < minN and myVector[i] > 0)
+			{
+				minN = myVector[i];
+				sveIndxMinN = i;
+			}
+			if (myVector[i] > max)
+			{
+				max = myVector[i];
+				sveIndxMax = i;
+			}
 			if (myVector[i] < min)
 			{
 				min = myVector[i];
+				sveIndxMin = i;
 			}
 		}
-		for (int i = 0; i < n; i++)
+		if (checks)
 		{
-			if (p == myVector[i])
-			{
-				cout << "The index of first " << p << " is " << i << endl;
-				break;
-			}
+			cout << "In the massive there is no " << p << endl
+				 << endl;
 		}
-		// cout << "The minimum value in the sequence is: " << min << endl;
+		cout << "The minvalue that is >0 :" << minN << "[" << sveIndxMinN << "]" << endl
+			 << endl;
+		cout << "The min value: " << min << "[" << sveIndxMin << "]" << endl;
+		cout << "The max value: " << max << "[" << sveIndxMax << "]" << endl
+			 << endl;
+		int backup;
+		backup = myVector[sveIndxMax];
+		myVector[sveIndxMax] = myVector[sveIndxMin];
+		myVector[sveIndxMin] = backup;
+		coutVec(myVector, n);
 	}
 
 	return 0;
